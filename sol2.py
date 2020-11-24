425daa07c7130d3408e725c935a859f8bc6e499b
Mohammad Saleh <mohammad.saleh2@cs.huji.ac.il>
2020-11-24 14:07:03 +0200

    sol2 updated

--- sol2.py
+++ sol2.py
@@ -1,1 +1,17 @@
-# starting tommorow
+import numpy as np
+
+
+def DFT(signal):
+    n = len(signal)
+    f_signal_arr = np.zeros((n,), np.complex128)
+    for u in range(n):
+        fourier_signal = 0
+        for x in range(n):
+            euler_exp = np.exp(-2 * np.pi * u * x * 1j * (1 / n))
+            fourier_signal += signal[x] * euler_exp
+        f_signal_arr[u] = fourier_signal
+    return f_signal_arr.astype(np.complex128)
+
+
+def IDFT(fourier_signal):
+    pass
