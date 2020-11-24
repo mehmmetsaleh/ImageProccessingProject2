d297969333d75e7e124a9ad3b49b2c28f8d9863d
Mohammad Saleh <mohammad.saleh2@cs.huji.ac.il>
2020-11-24 23:25:16 +0200

    DFT2 and IDFT2 added

--- sol2.py
+++ sol2.py
@@ -18,15 +18,32 @@ def DFT(signal):
     omega = np.exp(-2 * np.pi * 1j * (1 / n))
     arr = np.arange(n)
     dft_mat = np.vander(omega ** arr, increasing=True)
-    return np.dot(dft_mat, signal)
+    return np.dot(dft_mat, signal).astype(np.complex128)
 
 
 def IDFT(fourier_signal):
     n = len(fourier_signal)
     omega = np.exp(2 * np.pi * 1j * (1 / n))
     arr = np.arange(n)
-    idft_mat = np.vander(omega ** arr,increasing=True)
-    return (1/n) * np.dot(idft_mat,fourier_signal)
+    idft_mat = np.vander(omega ** arr, increasing=True)
+    res = (1 / n) * np.dot(idft_mat, fourier_signal)
+    return res.astype(np.complex128)
 
 
+def DFT2(image):
+    f_im = np.zeros(image.shape, dtype=np.complex128)
+    for col_idx in range(image.shape[1]):
+        f_im[:, col_idx] = DFT(image[col_idx])
+    for row_idx in range(image.shape[0]):
+        f_im[row_idx, :] = DFT(f_im[row_idx:, ])
+    return f_im.astype(np.complex128)
+
+
+def IDFT2(fourier_image):
+    im = np.zeros(fourier_image.shape, dtype=np.float64)
+    for col_idx in range(fourier_image.shape[1]):
+        im[:, col_idx] = IDFT(fourier_image[:, col_idx])
+    for row_idx in range(fourier_image.shape[0]):
+        im[row_idx,:] = IDFT(im[row_idx,:])
+    return im.astype(np.float64)
 
